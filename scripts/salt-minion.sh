998d9281d07171658133e24b6c7e4a15885ea94a
iamnswamyg <106902046+iamnswamyg@users.noreply.github.com>
2023-05-03 17:02:01 +0200

    Update salt-minion.sh

--- scripts/salt-minion.sh
+++ scripts/salt-minion.sh
@@ -6,7 +6,7 @@ fi
 
 if ! dpkg-query -W salt-minion >/dev/null 2>&1; then
   curl -L https://bootstrap.saltstack.com -o install_salt.sh
-  sudo sh install_salt.sh -P
+  sudo sh install_salt.sh -P stable 3005.1
 fi
 
 
