964040259b8aa05ffc192537095f40e5fefe9371
Narayan Swamy Ghanta <iamnswamy@outlook.com>
2023-05-04 18:17:52 +0200

    rvm

--- scripts/salt-mless-master.sh
+++ scripts/salt-mless-master.sh
@@ -14,9 +14,26 @@ if ! dpkg-query -W salt-master >/dev/null 2>&1; then
   sudo sh install_salt.sh stable 3005.1
   
   sudo apt update
-  sudo apt install gnupg2
-  gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-  \curl -sSL https://get.rvm.io | bash -s stable
+  sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev -y
+  curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+  source ~/.bashrc
+  ~/.rbenv/bin/rbenv install -l
+  ~/.rbenv/bin/rbenv install 3.2.2
+  ~/.rbenv/bin/rbenv global 3.2.2
+  rbenv global 3.2.2
+  ruby -v
+  echo "gem: --no-document" > ~/.gemrc
+  gem install bundler
+  gem update --system 3.4.12
+  gem env home
+  gem install rails 
+  rbenv rehash
+  rails -v
+  cd ~/.rbenv
+  git pull
+
     
 fi
 
