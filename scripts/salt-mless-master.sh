3df93c25c60822f083b6ad05de478f4028bbe1ca
iamnswamyg <106902046+iamnswamyg@users.noreply.github.com>
2023-05-04 17:25:40 +0200

    Update salt-mless-master.sh

--- scripts/salt-mless-master.sh
+++ scripts/salt-mless-master.sh
@@ -15,11 +15,9 @@ if ! dpkg-query -W salt-master >/dev/null 2>&1; then
   
   sudo apt update
   sudo apt install gnupg2
-  gpg2 --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-  \curl -sSL https://get.rvm.io -o rvm.sh
-  cat rvm.sh | bash -s stable --rails
-  source ~/.rvm/scripts/rvm
-  
+  gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  \curl -sSL https://get.rvm.io | bash -s stable
+    
 fi
 
 
