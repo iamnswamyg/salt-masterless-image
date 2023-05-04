7acc2fa3e663b7f19410f4c43afe2e7eb613b80a
iamnswamyg <106902046+iamnswamyg@users.noreply.github.com>
2023-05-04 16:42:06 +0200

    Update salt-mless-master.sh

--- scripts/salt-mless-master.sh
+++ scripts/salt-mless-master.sh
@@ -12,6 +12,15 @@ if ! dpkg-query -W salt-master >/dev/null 2>&1; then
 
   # Install Master
   sudo sh install_salt.sh stable 3005.1
+  
+  sudo apt install git autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev -y
+  curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+  source ~/.bashrc
+  ~/.rbenv/bin/rbenv install 3.2.2
+  ~/.rbenv/bin/rbenv global 3.2.2
+  ruby --version
 fi
 
 
